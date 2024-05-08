1b99ba68723ea0c9c0b241d5a113a7055e86a43b
creynolds8 <cameron8.cr@gmail.com>
2024-05-07 19:20:03 -0700

    add logic to skip inputs that are not numbers

--- timer1.js
+++ timer1.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 
 for (let i = 0; i < args.length; i++) {
-  if (args[i] > 0) {
+  if (args[i] > 0 && !Number.isNaN(args[i])) {
     setTimeout(() => {
       process.stdout.write('# \n');
     }, args[i] * 1000);
